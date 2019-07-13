546f3d4fda59eb04d8b9bdb3ccfb18d70b6efecb
Matt Gill <mtgill90@outlook.com>
2019-07-13 12:47:48 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-danger">Danger</button>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import ReactMap, { Layer, Feature } from 'react-mapbox-gl';
+
+const accessToken = 'pk.eyJ1IjoibXRnaWxsOTAiLCJhIjoiY2p5MHh5dm0wMDF0ZTNubzNkdDcyNXVndiJ9.C32ccynIS0v4UlaUylcoOQ';
+
+class App extends Component {
+  style = 'mapbox://styles/mapbox/streets-v11';
+
+  center = [-96, 37.8];
+
+  zoom = [4];
+
+  mapStyle = {
+    height: '75vh',
+    width: '75vw',
+  };
+
+  render() {
+    const Map = ReactMap({
+      accessToken,
+    });
+    return (
+      <div className="MapComp">
+        <Map
+        style={this.style}
+        containerStyle={this.mapStyle}
+        center={this.center}
+        zoom={this.zoom}
+      >
+       <Layer
+      type="symbol"
+      id="marker"
+      layout={{ 'icon-image': 'marker-15' }}>
+      <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+    </Layer>
+    </Map>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/MapBox/Map.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+class Map extends React.Component {
+
+  Map = ReactMapboxGl({
+    accessToken: 'pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A'
+  });
+
+
+  render() {
+    const map = <Map
+  style="mapbox://styles/mapbox/streets-v9"
+  containerStyle={{
+    height: '100vh',
+    width: '100vw',
+  }}>
+    <Layer
+      type="symbol"
+      id="marker"
+      layout={{ 'icon-image': 'marker-15' }}>
+      <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+    </Layer>
+</Map>;
+    return (
+      <div className="Map">
+        {map}
+      </div>
+
+    );
+  }
+}
+
+export default Map;
