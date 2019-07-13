a6771958dd65853ac602943eecf2abb756151bda
Matt Gill <mtgill90@outlook.com>
2019-07-13 18:38:56 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,21 @@
 
 import React from 'react';
-import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
 
-const accessToken = 'pk.eyJ1IjoibXRnaWxsOTAiLCJhIjoiY2p5MHh5dm0wMDF0ZTNubzNkdDcyNXVndiJ9.C32ccynIS0v4UlaUylcoOQ';
+import Map from '../components/MapBox/Map';
 
 class App extends React.Component {
-  style = 'mapbox://styles/mapbox/streets-v11';
-
-  center = [-86.7816, 36.1627];
-
-  zoom = 3;
-
-  mapStyle = {
-    height: '75vh',
-    width: '75vw',
-  };
+  state = {
+    position: [-86.7816, 36.1627],
+    zoom: [8],
+  }
 
   render() {
-    const Map = ReactMapboxGl({
-      accessToken,
-    });
+    const { position, zoom } = this.state;
     return (
-      <div className="MapComp">
+      <div className="App">
         <Map
-        style={this.style}
-        containerStyle={this.mapStyle}
-        center={this.center}
+        position={position}
+        zoom={zoom}
         />
       </div>
     );
--- src/components/MapBox/Map.js
+++ src/components/MapBox/Map.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Marker } from 'react-mapbox-gl';
+
+const accessToken = 'pk.eyJ1IjoibXRnaWxsOTAiLCJhIjoiY2p5MHh5dm0wMDF0ZTNubzNkdDcyNXVndiJ9.C32ccynIS0v4UlaUylcoOQ';
+
+const MapBox = ReactMapboxGl({
+  accessToken,
+});
 
 class Map extends React.Component {
-
-  Map = ReactMapboxGl({
-    accessToken: 'pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A'
-  });
-
+  style = 'mapbox://styles/mapbox/streets-v9';
 
   render() {
-    const map = <Map
-  style="mapbox://styles/mapbox/streets-v9"
-  containerStyle={{
-    height: '100vh',
-    width: '100vw',
-  }}>
-    <Layer
-      type="symbol"
-      id="marker"
-      layout={{ 'icon-image': 'marker-15' }}>
-      <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-    </Layer>
-</Map>;
+    const { position, zoom } = this.props;
     return (
       <div className="Map">
-        {map}
-      </div>
+        <MapBox
+        center={position}
+        zoom={zoom}
+        container='Map'
+        containerStyle={{
+          height: '100vh',
+          width: '100vw',
+        }}
+        style={this.style}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ 'icon-image': 'marker-15' }}>
+            </Layer>
+            <Marker
+  coordinates={[-86.7816, 36.1627]}
+  anchor="bottom">
+  <img href="../../../assets/pin.jpg" alt="..."/>
+</Marker>
+        </MapBox>
 
+      </div>
     );
   }
 }
